0b34858c37807f47093b7be0a7f25beea7ae8f26
Douglas Richardson <dougrich@protonmail.com>
2022-04-10 09:53:15 -0700

    feat: added WithEmbed modifier

--- message-modifier.go
+++ message-modifier.go
@@ -10,6 +10,8 @@ type MessageModifier interface {
 	Modify(*discordgo.InteractionResponse) error
 }
 
+// message modifier
+
 type messageModifier struct {
 	message string
 }
@@ -22,5 +24,22 @@ func (m *messageModifier) Modify(i *discordgo.InteractionResponse) error {
 func WithMessage(format string, a ...interface{}) MessageModifier {
 	return &messageModifier{
 		fmt.Sprintf(format, a...),
+	}
+}
+
+// embed modifier
+
+type embedModifier struct {
+	embed *discordgo.MessageEmbed
+}
+
+func (m *embedModifier) Modify(i *discordgo.InteractionResponse) error {
+	i.Data.Embeds = append(i.Data.Embeds, m.embed)
+	return nil
+}
+
+func WithEmbed(embed *discordgo.MessageEmbed) MessageModifier {
+	return &embedModifier{
+		embed,
 	}
 }
